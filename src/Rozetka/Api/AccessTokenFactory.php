8c4592c9c811e79ac35484d2675b1dfd0dfae3cc
Oleksandr D <tarpedo10@gmail.com>
2023-10-17 21:24:39 +0300

    Setup memcached

--- src/Rozetka/Api/AccessTokenFactory.php
+++ src/Rozetka/Api/AccessTokenFactory.php
@@ -6,7 +6,10 @@ namespace App\Rozetka\Api;
 
 class AccessTokenFactory
 {
+    private const CACHE_LIFETIME = 30;
+
     public function __construct(
+        private readonly \App\Rozetka\Account\Credentials\Retrieve $credentialsRetrieve,
         private readonly \Symfony\Contracts\Cache\CacheInterface $cache,
     ) {
     }
@@ -14,14 +17,23 @@ class AccessTokenFactory
     public function create(\App\Rozetka\Account $account): string
     {
         return $this->cache->get(
-            $this->getCacheKey($account->getId()),
-            function (\Symfony\Contracts\Cache\ItemInterface $item) use ($account) {
-                return '';
+            $this->prepareCacheKey($account->getId()),
+            function (\Symfony\Contracts\Cache\ItemInterface $item, &$save) use ($account) {
+                $item->expiresAfter(self::CACHE_LIFETIME);
+
+                $accountInfo = $this->credentialsRetrieve->process($account->getUsername(), $account->getPassword());
+
+                if ($accountInfo->get('success') === false) {
+                    $save = false;
+                    return null;
+                }
+
+                return $accountInfo->get('content/access_token');
             },
         );
     }
 
-    private function getCacheKey(int $accountId): string
+    private function prepareCacheKey(int $accountId): string
     {
         return "app.rozetka.api.access_token.{$accountId}";
     }
