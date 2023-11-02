f6c6aa63a38d03d96bf40351e6a199a1ec0a5e02
Oleksandr D <tarpedo10@gmail.com>
2023-11-03 00:00:29 +0200

    fix

--- src/InternalApi/ControllerInterface.php
+++ src/InternalApi/ControllerInterface.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\InternalApi;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
 interface ControllerInterface
 {
+    public function process(Request $request): JsonResponse;
 }
